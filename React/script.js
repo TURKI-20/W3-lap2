23ce6ab0e01f99644c093a0089fb80ceffc4a08b
TURKI-20 <109510162+TURKI-20@users.noreply.github.com>
2023-01-30 16:54:14 +0300

    Add files via upload

---
+++ React/script.js
@@ -0,0 +1,137 @@
+// const name1= document.getElementById("root");
+// const element= document.createElement("div");
+// element.innerHTML="hello world";
+// name1.appendChild(element);
+
+
+// const container = document.getElementById("root");
+
+// const element= React.createElement("div",{
+//     children: "Hello",
+// });
+// ReactDOM.render(element, container);
+
+// –––––––––––––––––––––––––––––––––
+
+const container1 = document.getElementById("CV");
+let element1 = 
+<>
+<link rel="stylesheet" type="text/css" href="resume.css" media="all" />
+
+
+
+<div id="doc2" class="yui-t7">
+<div id="inner">
+
+<div id="hd">
+<div class="yui-gc">
+<div class="yui-u first">
+<h1>Turki Fahad Alboaireed</h1>
+<h2>Web developer</h2>
+</div>
+
+</div>
+</div>
+
+<div id="bd">
+<div id="yui-main">
+<div class="yui-b">
+
+<div class="yui-gf">
+<div class="yui-u first">
+<h2>Profile</h2>
+</div>
+<div class="yui-u">
+<p class="enlarge">
+Graduate in computer science .I am eager to secure a job opportunity using my experience and knowledge . I am excited to be a part of the organization 
+</p>
+</div>
+</div>
+<div class="yui-gf last">
+<div class="yui-u first">
+<h2>Education</h2>
+</div>
+<div class="yui-u">
+<h2>Bachelor of Computer Science</h2>
+<h3>Al-Imam Mohammed bin Saud University | 2017 - 2022</h3>
+</div>
+</div>
+
+<div class="yui-gf">
+<div class="yui-u first">
+<h2>Experience</h2>
+</div>
+
+<div class="yui-u">
+
+<div class="job">
+    </div>
+    <h2>Ministry of Municipal and Rural affairs and Housing</h2>
+<h2>Development Department - Agency for Digital transformation and smart cities</h2>
+                                
+</div>
+</div>
+
+
+<div class="yui-gf">
+<div class="yui-u first">
+<h2>Technical</h2>
+</div>
+<div class="yui-u">
+<ul class="talent">
+    <li>Java</li>
+    <li>HTML</li>
+    <li class="last">CSS</li>
+</ul>
+
+<ul class="talent">
+    <li>Bootstrap</li>
+    <li>Javascript</li>
+    <li class="last">PHP</li>
+</ul>
+</div>
+</div>
+
+
+
+
+<div class="yui-gf">
+<div class="yui-u first">
+<h2>Soft Skills</h2>
+</div>
+<div class="yui-u">
+    <div class="talent">
+        <h2>Problem Solving</h2>
+    </div>
+    <div class="talent">
+        <h2>Communication</h2>
+    </div>
+    <div class="talent">
+        <h2>Teamwork</h2>
+    </div>
+</div>
+</div>
+
+
+
+
+
+</div>
+</div>
+</div>
+
+<div id="ft">
+<p>Turki Fahad &mdash; <a href="turki-fahad1996@gmail.com">turki-fahad1996@gmail.com</a> &mdash; +966555555</p>
+</div>
+
+</div>
+
+
+</div>
+
+
+<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.7.0/build/reset-fonts-grids/reset-fonts-grids.css" media="all" /> 
+
+
+</>
+ReactDOM.render(element1 , container1);
